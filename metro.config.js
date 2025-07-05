c250e1af16458179e0bac39fd8934e3168e2a7c4
olekmorawski <olek.morawski@gmail.com>
2025-07-05 23:55:33 +0200

    ale brainrot

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,16 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname)
 
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+    try {
+        return context.resolveRequest(context, moduleName, platform)
+    } catch (error) {
+        if (moduleName.endsWith('.js')) {
+            const tsModuleName = moduleName.replace(/\.js$/, '.ts')
+            return context.resolveRequest(context, tsModuleName, platform)
+        }
+        throw error
+    }
+}
+
 module.exports = withNativeWind(config, { input: './global.css' })
