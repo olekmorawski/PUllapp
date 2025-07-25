9c13dacfce057ebbeebb344aa01d8e78d40a993e
olekmorawski <olek.morawski@gmail.com>
2025-07-25 15:09:32 +0200

    complexify build

--- constants/Tokens.ts
+++ constants/Tokens.ts
@@ -1,1 +1,1 @@
-export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoib2xla21vcmF3c2tpIiwiYSI6ImNtY21jaXZnYTBnaTAybHMzMWp4cnB2MmYifQ.gmAgP14PfVyDhelFkHeFsg';
+export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoib2xla21vcmF3c2tpIiwiYSI6ImNtZGkwcmczYTAwZmoyanM1eGdzZWh4NGsifQ.xHnSlxwo27wFFs2o6-YsTg';
